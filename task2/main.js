68143d7d57e4b0ec97ee0c4cc1a03fc27ce7089f
Sergey <berdnikausiarhei@gmail.com>
2019-04-01 13:47:24 +0300

    lesson-4

---
+++ task2/main.js
@@ -0,0 +1,12 @@
+$button = document.querySelector('.btn-test');
+console.log($button);
+$textIn = document.querySelector('.text-in').textContent;
+$textOut = document.querySelector('.text-out');
+
+$button.addEventListener('click', (event) => {
+    if(event.target.classList.contains('btn-test')){
+        const outText = $textIn.replace(/(?<=\W)'|'(?=\W)/gi, '\"');
+        $textOut.textContent = outText; 
+    }
+} );
+
